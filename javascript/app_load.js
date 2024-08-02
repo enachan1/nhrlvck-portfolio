695b56336fc751ea6c9f256de5372bd60e39e945
enachan1 <yourmotherknowsbest022@gmail.com>
2024-08-02 14:15:41 +0800

    fixes layout

--- javascript/app_load.js
+++ javascript/app_load.js
@@ -1,6 +1,6 @@
 const app_load = () => {
   const basePath = window.location.pathname.startsWith('/nhrlvck-portfolio') ? '/nhrlvck-portfolio' : '';
 
-    particlesJS.load('particles-js', `${basePath}/particlesjs-config.json`);
+    particlesJS.load('particles-js', `${basePath}/json/particlesjs-config.json`);
 }
 export default app_load;
